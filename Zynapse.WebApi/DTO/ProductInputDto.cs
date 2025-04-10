404c359aeee9e4ab76c4dfcaefb35e5a6f06e5b8
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-04-10 18:24:59 +0200

    Add ProductInputDto to handle product input data

---
+++ Zynapse.WebApi/DTO/ProductInputDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Zynapse.WebApi.DTO;
+
+public record ProductInputDto
+{
+    [Required]
+    [MinLength(2)]
+    [MaxLength(100)]
+    [DefaultValue("World of Warcraft")]
+    public required string Name { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+}
